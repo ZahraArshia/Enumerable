55754346a3bbb7dba9200afd74a06c1c1f2d9896
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-05-12 18:54:50 +0430

    create enumerable module

---
+++ MyEnumerable.rb
@@ -0,0 +1,23 @@
+module MyEnumerable
+  def any?
+    each {
+      |item| return true if yield item 
+    }
+    false
+  end
+
+  def all?
+    each {
+      |item| return false unless yield item
+    }
+    true
+  end
+
+  def filter 
+    filtered_list = []
+    each {
+      |item| filtered_list.push(item) if yield item
+    }
+    return filtered_list
+  end
+end
